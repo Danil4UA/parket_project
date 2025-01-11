340c041dc25b2bdc21c708b240c40e39ae23061c
Danil4UA <146213395+Danil4UA@users.noreply.github.com>
2025-01-11 22:54:34 +0200

    add basis for filtering prodcuts

--- parket-front-end/src/app/products/page.tsx
+++ parket-front-end/src/app/products/page.tsx
@@ -1,9 +1,12 @@
+import ProductsFilter from "@/components/Products/ProductsFilter/ProductsFilter"
 import ProductsList from "@/components/Products/ProductsList/ProductsList"
+import "./products.css"
 
 const ProductsPage = () => {
     return (
         <>
-            <div>
+            <div className="products-page-container">
+                <ProductsFilter />
                 <ProductsList />
             </div>
         </>
---
+++ parket-front-end/src/components/Products/ProductsFilter/ProductsFilter.tsx
@@ -0,0 +1,12 @@
+import "./ProductsFilter.css"
+const ProductsFilter = () => {
+    return (
+        <>
+            <div className="ProductsFilter">
+
+            </div>
+        </>
+    )
+}
+
+export default ProductsFilter
