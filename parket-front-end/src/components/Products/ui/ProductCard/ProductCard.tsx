d78f40596507d54569f2a19c0afc9823610c7f1e
Danil4UA <146213395+Danil4UA@users.noreply.github.com>
2025-01-21 23:37:45 +0200

    add styles to cards, add button inside the card

--- parket-front-end/src/components/Products/ui/ProductCard/ProductCard.tsx
+++ parket-front-end/src/components/Products/ui/ProductCard/ProductCard.tsx
@@ -7,10 +7,10 @@ interface ProductCardProps {
   productId: string;
   productName: string;
   productPrice: string;
-  productDescription: string;
+  productDescription?: string;
 }
 
-const ProductCard = ({ productId, productName, productPrice, productDescription }: ProductCardProps) => {
+const ProductCard = ({ productId, productName, productPrice }: ProductCardProps) => {
   return (
     <div className={classNames("ProductCard", {}, [])}>
       <Link href={`/products/${productId}`} className="card__media">
@@ -23,9 +23,18 @@ const ProductCard = ({ productId, productName, productPrice, productDescription 
             />
         </div>
         <div className="card__information">
-          <h2>{productName}</h2>
-          <p>Price: {productPrice}</p>
-          <p>{productDescription}</p>
+          <div>{productName}</div>
+          <div className="card__information_price">
+            <span className="prefix">₪</span>
+            {productPrice}
+            <span className="price__suffix">
+              .00
+            </span>
+
+          </div>
+          <div className="card__information_button">
+            Get More details
+          </div>
         </div>
       </Link>
     </div>
